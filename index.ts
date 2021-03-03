a8215c7b6e26b00f5a9ba573d486821cb82cfe5a
McTano <tsmackinlay@gmail.com>
2021-03-03 13:39:50 -0800

    2 files modified, 1 added

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import "./style.css";
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById("app") as HTMLElement;
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>Make A Shield</h1>`;
 
 type Field = Tincture;
 type Tincture = string;
